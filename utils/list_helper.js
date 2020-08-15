87bd5c47a360ddb584237a3045caadca532fbc81
arskdj <68353557+arskdj@users.noreply.github.com>
2020-08-16 00:15:58 +0300

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,17 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favouriteBlog = (blogs) => { 
+    const maxReducer = (max, blog, index) => { 
+        return blogs[max].likes < blog.likes ? index : max
+    }
+    
+    const max_index = blogs.reduce(maxReducer, 0) 
+    return blogs[max_index]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
